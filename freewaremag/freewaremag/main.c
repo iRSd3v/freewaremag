d52a075b49a812003061653061c9196c0666774d
iRS <irs.dev@icloud.com>
2022-06-18 00:41:52 +0200

    Test

---
+++ freewaremag/freewaremag/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  freewaremag
+//
+//  Created by Rafał Stefaniak on 18/06/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
